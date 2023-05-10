1fec5c8ee9e667517e78de57b5f51d9d70a95d33
imshukla12 <shuklaakanksha1298@gmail.com>
2023-05-10 21:46:56 +0530

    homepage updated

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -23,7 +23,7 @@ const HomePage = () => {
           <img src={img} alt='image_homepage' className="h-1/6 max-w-full" />
         </div>
         <div className='flex flex-col w-1/2 justify-center space-y-6 items-center p-4'>
-          <div><p className='font-serif text-5xl text-white'>Welcome to our E-RaktKendra</p></div>
+          <div><p className='font-serif text-5xl text-white'>Welcome to E-RaktKendra</p></div>
           <div className='flex flex-row w-1/2 justify-evenly space-x-6 items-center mt-6 p-4'>
             <button className='bg-white text-red-600 rounded-lg px-4 py-2 transform transition duration-300 hover:bg-red-200 hover:scale-125' onClick={handleUser}>User</button>
             <button className='bg-white text-red-600 rounded-lg px-2 py-2 transform transition duration-300 hover:bg-red-200 hover:scale-125' onClick={handleFieldWorker}>FieldWorker</button>
